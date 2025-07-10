cbd05865cfc0c5ce2c4923e29016b9e668336f47
Samuel Bosch-Bird <boschbsa@mail.uc.edu>
2025-07-10 19:00:10 -0400

    add name controller

---
+++ GitOut/Controllers/NameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        private readonly ILogger<NameController> _logger;
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetName")]
+
+        public IActionResult Get()
+        {
+            var returnName = new Name("Samuel");
+            return Ok(returnName);
+        }
+    }
+}
---
+++ GitOut/Name.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut
+{
+    public class Name : Controller
+    {
+        public string FirstName { get; set; }
+
+        public Name(string FirstName) { this.FirstName = FirstName; }
+    }
+}
